72dc08bbf3ee7a26a8060de929a17b0ec109e647
sammybolye <58224695+sammybolye@users.noreply.github.com>
2022-06-14 12:01:19 +0100

    Update install-kf-generic.sh

--- install-kf-generic.sh
+++ install-kf-generic.sh
@@ -1,6 +1,6 @@
 export PROJECT_ID=`gcloud config get-value project` &&   export M_TYPE=n1-standard-2 &&   export ZONE=us-central1-b &&   export CLUSTER_NAME=${PROJECT_ID}-${RANDOM} &&   gcloud services enable container.googleapis.com &&   gcloud container clusters create $CLUSTER_NAME   --cluster-version 1.20.15   --machine-type=$M_TYPE   --num-nodes 4   --zone $ZONE   --project $PROJECT_ID
 
-kubectl get pods -A -o=custom-columns='DATA:spec.containers[*].image'| sort |uniq > before-$1.list
+
 
 wget https://github.com/kubeflow/manifests/archive/refs/heads/v$1-branch.zip
 
@@ -10,7 +10,6 @@ cd manifests-$1-branch/
 
 while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
 
-kubectl get pods -A -o=custom-columns='DATA:spec.containers[*].image' |sort |uniq > after-$1.list
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=example Inc./CN=example.com' -keyout example.com.key -out example.com.crt
 openssl req -out httpbin.example.com.csr -newkey rsa:2048 -nodes -keyout tls.key -subj "/CN=httpbin.example.com/O=httpbin organization"
 openssl x509 -req -days 365 -CA example.com.crt -CAkey example.com.key -set_serial 0 -in httpbin.example.com.csr -out tls.crt
@@ -19,8 +18,12 @@ kubectl create secret generic istio-ingressgateway-certs --from-file=tls.key=tls
 
 kubectl patch svc istio-ingressgateway -n istio-system -p '{"metadata":{"annotations":{"beta.cloud.google.com/backend-config":"{\"ports\": {\"http2\":\"iap-backendconfig\"}}"}}}'
 
+
 kubectl patch svc istio-ingressgateway -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
 
+kubectl patch MutatingWebhookConfiguration istio-sidecar-injector -p '{"metadata":{"annotations":{"admissions.enforcer/disabled": "true"}}}'
+
+kubectl delete pod --all -n kubeflow
 
 kubectl apply -f - <<EOF
 apiVersion: networking.istio.io/v1alpha3
