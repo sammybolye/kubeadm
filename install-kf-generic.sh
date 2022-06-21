58022343123162382e8bc3879e397ab59c4c60a0
sammybolye <58224695+sammybolye@users.noreply.github.com>
2022-06-21 17:05:50 +0100

    Update install-kf-generic.sh

--- install-kf-generic.sh
+++ install-kf-generic.sh
@@ -1,4 +1,4 @@
-export PROJECT_ID=`gcloud config get-value project` &&   export M_TYPE=n1-standard-2 &&   export ZONE=us-central1-b &&   export CLUSTER_NAME=${PROJECT_ID}-${RANDOM} &&   gcloud services enable container.googleapis.com &&   gcloud container clusters create $CLUSTER_NAME   --cluster-version 1.20.15   --machine-type=$M_TYPE   --num-nodes 4   --zone $ZONE   --project $PROJECT_ID
+export PROJECT_ID=`gcloud config get-value project` &&   export M_TYPE=n1-standard-2 &&   export ZONE=us-central1-b &&   export CLUSTER_NAME=${PROJECT_ID}-${RANDOM} &&   gcloud services enable container.googleapis.com &&   gcloud container clusters create $CLUSTER_NAME   --cluster-version 1.20.15   --machine-type=$M_TYPE   --num-nodes 4   --zone $ZONE   --project $PROJECT_ID --workload-pool=$PROJECT_ID.svc.id.goog
 
 
 
